3abc9a5558bc249e8725d51ed93f64c2fc9767f1
JoshuaSeth <SB@accette.eu>
2023-05-08 15:57:50 +0200

    docs again

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,56 @@ import subprocess
 from spacy import displacy
 from st_helpers import merge_words_and_entities, visualise_dsouza_parser, visualize_dygie_parser, visualize_mechanic_granular_parser, SchemaParser
 
+
+# Helper functions
+
+
+def load_sentences(use_both):
+    prefix = ''
+    if use_both:
+        prefix = 'pred_'
+
+    with open(f'ORKG_parsers/dygiepp/predictions/{prefix}scierc.jsonl', 'r') as f:
+        data = f.read()
+        data = eval(data)
+        sentences = data['sentences']
+    return prefix, sentences
+
+
+def load_data_for_selected_models(options, prefix):
+    model_data = {}
+    for option in options:
+        if option != "D'Souza's CL-TitleParser":
+            model = option.replace('MECHANIC-', '').replace('05',
+                                                            '').split()[1].lower()
+
+        # Data is preparsed and pickled and loaded here (so not parsed on the fly)
+
+            with open(f'ORKG_parsers/dygiepp/predictions/{prefix}{model}.jsonl', 'r') as f:
+                data = f.read()
+            data = eval(data)
+
+            model_data[model] = data
+    return model_data
+
+
+def merge_two_merged_lists(merged_list1, merged_list2):
+    result = merged_list1.copy()
+    i = 0
+    j = 0
+
+    while i < len(result) and j < len(merged_list2):
+        if isinstance(result[i], str) and isinstance(merged_list2[j], tuple):
+            if result[i] == merged_list2[j][0]:
+                result[i] = merged_list2[j]
+                j += 1
+        i += 1
+
+    return result
+
+# ----- ----- ----- ----- ----- ----- ----- ---- ----
+
+
 # Default interface options
 st.header('NER & RE Parsing Results')
 
@@ -24,6 +74,9 @@ options = st.multiselect(
 use_both = st.checkbox(
     'Use challenges OR directions vs. use challenges AND directions')
 
+compare_merge = st.checkbox(
+    'Compare parsers results or merge parser results')
+
 st.divider()
 
 # ----- ----- ----- ----- ----- ----- ----- ---- ----
@@ -32,50 +85,46 @@ st.divider()
 
 
 schema_parser = SchemaParser()
-prefix = ''
-if use_both:
-    prefix = 'pred_'
 
-with open(f'ORKG_parsers/dygiepp/predictions/{prefix}scierc.jsonl', 'r') as f:
-    data = f.read()
-    data = eval(data)
-    sentences = data['sentences']
+# Load sentences either challenging OR direction or challenge AND direction future research
+prefix, sentences = load_sentences(use_both)
 
-model_data = {}
+model_data = load_data_for_selected_models(options, prefix)
 
-for option in options:
-    if option != "D'Souza's CL-TitleParser":
-        model = option.replace('MECHANIC-', '').replace('05',
-                                                        '').split()[1].lower()
-
-        # Data is preparsed and pickled and loaded here (so not parsed on the fly)
-
-        with open(f'ORKG_parsers/dygiepp/predictions/{prefix}{model}.jsonl', 'r') as f:
-            data = f.read()
-        data = eval(data)
-
-        model_data[model] = data
-
-
+# For each sentence apply all selected models
 for idx, s in enumerate(sentences):
+    # Layout
+    st.markdown('\n')
+    st.subheader(str(idx))
+
+    ents_for_models = []
     for option in options:
-        model = option.replace('MECHANIC-', '').replace('05',
-                                                        '').split()[1].lower()
-
-        print(model)
-
-        data = model_data[model] if not model == 'cl-titleparser' else None
-
         st.markdown('\n')
-        st.subheader(str(idx))
+        st.caption(option)
+
+        # Load pre-parsed data
+        model_name = option.replace(
+            'MECHANIC-', '').replace('05', '').split()[1].lower()
+
+        data = model_data[model_name] if not model_name == 'cl-titleparser' else None
+
+        # Get idx of first word of sentence in the total text
         sent_start_idx = sum([len(sent)
                               for sent in sentences[:idx]])
 
+        # Entities
         parsed_ents = schema_parser.parse_ents(
-            s, model, data, idx, sent_start_idx)
+            s, model_name, data, idx, sent_start_idx)
+        ents_for_models.append(parsed_ents)
         annotated_text(parsed_ents)
 
+        # Relations
         parsed_rels = schema_parser.parse_rels(
-            model, data, s, idx, sent_start_idx)
+            model_name, data, s, idx, sent_start_idx)
         for rel in parsed_rels:
             st.text(rel)
+
+    if compare_merge:
+        res = merge_two_merged_lists(ents_for_models[0], ents_for_models[1])
+
+        annotated_text(res)
